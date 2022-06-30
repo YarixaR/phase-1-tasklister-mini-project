ad281c2ba089a56abf6e4ffaff9b24c0ee8e0f50
Yarixa Rodriguez <yarixarodriguez94@gmail.com>
2022-06-29 21:21:36 -0400

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  taskForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+  });
 });
